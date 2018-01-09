c588a34bf5c4b27e262a6c588a543592d162ef87
lijinchuan <403479851@qq.com>
2018-01-09 14:15:27 +0800

    TimeOutException: message->timeout

--- Ljc.JFramework/src/Ljc/JFramework/TimeOutException.java
+++ Ljc.JFramework/src/Ljc/JFramework/TimeOutException.java
@@ -8,7 +8,7 @@ public class TimeOutException extends CoreException {
 	private static final long serialVersionUID = 1L;
 
 	public TimeOutException() {
-		super();
+		super("time out");
 	}
 
 	public TimeOutException(String message) {
